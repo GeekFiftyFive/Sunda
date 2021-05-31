86e9747b94c261bfa7890c667a8f84a89df938c1
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-05-31 12:07:31 +0100

    Tokenise simple where conditions

--- src/Tokeniser/Tokeniser.test.ts
+++ src/Tokeniser/Tokeniser.test.ts
@@ -6,8 +6,18 @@ describe('test tokeniser', () => {
     expect(actual).toEqual(['SELECT', '*', 'FROM', 'table']);
   });
 
+  test('tokenise valid simple command in lowercase', () => {
+    const actual = tokenise('select * from table');
+    expect(actual).toEqual(['select', '*', 'from', 'table']);
+  });
+
   test('tokenise valid simple command with leading whitspace', () => {
     const actual = tokenise('  SELECT * FROM table');
     expect(actual).toEqual(['SELECT', '*', 'FROM', 'table']);
+  });
+
+  test('tokenise valid simple command with simple where condition', () => {
+    const actual = tokenise('SELECT * FROM table WHERE field="value";');
+    expect(actual).toEqual(['SELECT', '*', 'FROM', 'table', 'WHERE', 'field', '=', '"value"', ';']);
   });
 });
--- src/Tokeniser/Tokeniser.ts
+++ src/Tokeniser/Tokeniser.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 export const tokenise = (input: string): string[] => {
-  const regex = /[\s,]*(SELECT|FROM|[^\s\\]+)/gm;
+  const regex = /[\s,]*(SELECT|FROM|WHERE|\*|\w+|=|;|".*")/gmi;
   return [...input.matchAll(regex)].map((match) => match[1]);
 };
