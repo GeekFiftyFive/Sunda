c91f55590e91827b75c0e390f21820967d501b49
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-06-02 18:12:38 +0100

    Add a repl

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
+/* eslint-disable no-console */
+import * as readline from 'readline';
+import * as fs from 'fs';
 import { execute } from './Executor';
 import { parse } from './Parser';
 import { tokenise } from './Tokeniser';
+
+if (process.argv.length < 3) {
+  console.error('Usage: squirrel <input-path>');
+  process.exit(1);
+}
+
+const inputPath = process.argv[2];
+const dataset = JSON.parse(fs.readFileSync(inputPath, 'utf8'));
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 export const executeQuery = <T>(query: string, data: Record<string, unknown[]>): T[] => {
   const tokens = tokenise(query);
   const parsedQuery = parse(tokens);
   return execute<T>(parsedQuery, data);
 };
+
+rl.setPrompt('squirrel> ');
+rl.prompt();
+rl.on('line', (input: string) => {
+  console.log(executeQuery(input, dataset));
+  rl.prompt();
+});
+rl.on('close', process.exit);
