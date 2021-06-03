3f5907fb1588b96a6104d9f563883ea7d52add75
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-06-03 20:38:12 +0100

    Simplify tokeniser regex

--- src/Tokeniser/Tokeniser.ts
+++ src/Tokeniser/Tokeniser.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 export const tokenise = (input: string): string[] => {
-  const regex = /[\s,]*(SELECT|FROM|WHERE|BETWEEN|LIKE|AND|OR|NOT|\*|[0-9]+\.[0-9]+|\w+|<=|>=|<>|<|>|=|;|"[^".]*")/gmi;
+  const regex = /[\s,]*(\*|[0-9]+\.[0-9]+|\w+|<=|>=|<>|<|>|=|;|"[^".]*")/gmi;
   return [...input.matchAll(regex)].map((match) => match[1]);
 };
