942623907a28887ba140022b2697eedfd72a83ad
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-11-03 20:30:41 +0000

    Implement simple args parser

---
+++ src/ArgsParser/ArgsParser.test.ts
@@ -0,0 +1,79 @@
+import { createArgsParser } from '.';
+
+describe('ArgsParser', () => {
+  test('Value is used as default when not proceeded by a flag', () => {
+    const parser = createArgsParser([
+      {
+        key: 'default',
+        default: true,
+      },
+    ]);
+
+    expect(parser(['default value'])).toEqual({
+      default: 'default value',
+    });
+  });
+
+  test('Longhand flags are properly parsed', () => {
+    const parser = createArgsParser([
+      {
+        key: 'flag1',
+        longhand: '--flag_one',
+      },
+      {
+        key: 'flag2',
+        longhand: '--flag_two',
+      },
+    ]);
+
+    expect(parser(['--flag_one', 'flag 1 value', '--flag_two', 'flag 2 value'])).toEqual({
+      flag1: 'flag 1 value',
+      flag2: 'flag 2 value',
+    });
+  });
+
+  test('Shorthand flags are properly parsed', () => {
+    const parser = createArgsParser([
+      {
+        key: 'flag1',
+        shorthand: '--f1',
+      },
+      {
+        key: 'flag2',
+        shorthand: '--f2',
+      },
+    ]);
+
+    expect(parser(['--f1', 'flag 1 value', '--f2', 'flag 2 value'])).toEqual({
+      flag1: 'flag 1 value',
+      flag2: 'flag 2 value',
+    });
+  });
+
+  test('A mixture of flags parses', () => {
+    const parser = createArgsParser([
+      {
+        key: 'flag1',
+        shorthand: '--f1',
+        longhand: '--flag_one',
+      },
+      {
+        key: 'flag2',
+        shorthand: '--f2',
+        longhand: '--flag_two',
+      },
+      {
+        key: 'default',
+        default: true,
+      },
+    ]);
+
+    expect(parser(['default value', '--f1', 'flag 1 value', '--flag_two', 'flag 2 value'])).toEqual(
+      {
+        flag1: 'flag 1 value',
+        flag2: 'flag 2 value',
+        default: 'default value',
+      },
+    );
+  });
+});
---
+++ src/ArgsParser/ArgsParser.ts
@@ -0,0 +1,53 @@
+export interface ArgInput {
+  key: string;
+  default?: boolean;
+  longhand?: string;
+  shorthand?: string;
+}
+
+export const createArgsParser =
+  (argInputs: ArgInput[]): ((args: string[]) => Record<string, string>) =>
+  (args: string[]) => {
+    const [shorthandMap, longhandMap] = argInputs.reduce(
+      ([shorthandMap, longhandMap], current) => {
+        if (current.shorthand) {
+          // eslint-disable-next-line no-param-reassign
+          shorthandMap[current.shorthand] = current.key;
+        }
+
+        if (current.longhand) {
+          // eslint-disable-next-line no-param-reassign
+          longhandMap[current.longhand] = current.key;
+        }
+
+        return [shorthandMap, longhandMap];
+      },
+      [{} as Record<string, string>, {} as Record<string, string>],
+    );
+    const defaultKey = argInputs.find((input) => input.default)?.key;
+    let param: string;
+    const map = args.reduce((mapped, arg) => {
+      if (longhandMap[arg] || shorthandMap[arg]) {
+        param = arg;
+        return mapped;
+      }
+
+      if (defaultKey && !param) {
+        // eslint-disable-next-line no-param-reassign
+        mapped[defaultKey] = arg;
+      }
+
+      if (param) {
+        const key = longhandMap[param] || shorthandMap[param];
+        if (!key) {
+          throw new Error(`Invalid parameter '${param}'`);
+        }
+        // eslint-disable-next-line no-param-reassign
+        mapped[key] = arg;
+        param = undefined;
+      }
+
+      return mapped;
+    }, {} as Record<string, string>);
+    return map;
+  };
