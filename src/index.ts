3101f0be6701a681ea5a3083a179f4ab8195dd71
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-11-03 21:59:24 +0000

    Implement ability to read from and write to stdin and stdout as well as a specified output location

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { tokenise } from './Tokeniser';
 import { read } from './Reader';
 import { createObjectDataSource } from './ObjectDataSource';
 import { DataSource } from './CommonTypes';
+import { createArgsParser } from './ArgsParser';
 
 export * from './CommonTypes';
 
@@ -23,12 +24,7 @@ export const executeQueryFromObject = async <T>(
   data: Record<string, unknown[]>,
 ): Promise<T[]> => executeQuery<T>(query, createObjectDataSource(data));
 
-if (require.main === module) {
-  if (process.argv.length < 3) {
-    console.error('Usage: sunda <input-path>');
-    process.exit(1);
-  }
-  const inputPath = process.argv[2];
+const startRepl = (inputPath: string) => {
   const dataset = read(fs.readFileSync(inputPath, 'utf8'));
 
   const rl = readline.createInterface({
@@ -47,4 +43,66 @@ if (require.main === module) {
     rl.prompt();
   });
   rl.on('close', process.exit);
+};
+
+const startStreamMode = async (query: string, inputPath?: string, outputPath?: string) => {
+  const readStream = inputPath ? fs.createReadStream(inputPath) : process.stdin;
+  const writeStream = outputPath ? fs.createWriteStream(outputPath) : process.stdout;
+
+  const data = await new Promise<Record<string, unknown[]>>((resolve, reject) => {
+    let accumulated = '';
+    readStream.on('data', (chunk) => {
+      accumulated += chunk.toString();
+    });
+    readStream.on('close', () => {
+      resolve(JSON.parse(accumulated));
+    });
+    readStream.on('error', reject);
+  });
+
+  const queryResult = await executeQueryFromObject(query, data);
+
+  await new Promise((resolve, reject) => {
+    writeStream.write(JSON.stringify(queryResult));
+    writeStream.on('finish', resolve);
+    writeStream.on('error', reject);
+  });
+
+  writeStream.end();
+};
+
+if (require.main === module) {
+  if (process.argv.length < 3) {
+    console.error('Usage: sunda <input-path>');
+    process.exit(1);
+  }
+
+  const argsParser = createArgsParser([
+    {
+      key: 'inputPath',
+      default: true,
+    },
+    {
+      key: 'outputPath',
+      longhand: '--output',
+      shorthand: '-o',
+    },
+    {
+      key: 'query',
+      longhand: '--query',
+      shorthand: '-q',
+    },
+  ]);
+  const { outputPath, inputPath, query } = argsParser(process.argv.slice(2));
+
+  if (outputPath && inputPath && !query) {
+    console.error('Cannot specify output location when running in REPL mode');
+    process.exit(1);
+  }
+
+  if (!query) {
+    startRepl(inputPath);
+  } else {
+    startStreamMode(query, inputPath, outputPath);
+  }
 }
