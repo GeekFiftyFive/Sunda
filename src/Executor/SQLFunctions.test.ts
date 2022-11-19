61865ee8d30ea9dadf5dfe851f0c9421a06b6237
GeekFiftyFive <amylouisepellatt@gmail.com>
2022-11-19 14:07:42 +0000

    Add test coverage for the REGEX_GROUP function

--- src/Executor/SQLFunctions.test.ts
+++ src/Executor/SQLFunctions.test.ts
@@ -19,4 +19,24 @@ describe("test 'ARRAY_POSITION' function", () => {
   });
 });
 
-// TODO: Test coverage for REGEX_GROUP
+describe("test 'REGEX_GROUP' function", () => {
+  test('test correctly match a present match group in index 1', () => {
+    const actual = functions.REGEX_GROUP('^Hello, (\\w*)(!)$', 'Hello, world!', 1);
+    expect(actual).toEqual('world');
+  });
+
+  test('test fail to match a non-persent match group in index 1', () => {
+    const actual = functions.REGEX_GROUP('^Hello, (\\w*)(!)$', 'Not present in regex', 1);
+    expect(actual).toEqual(undefined);
+  });
+
+  test('test correctly match a present match group in index 2', () => {
+    const actual = functions.REGEX_GROUP('^Hello, (\\w*)(!)$', 'Hello, world!', 2);
+    expect(actual).toEqual('!');
+  });
+
+  test('test fail to match a non-persent match group in index 1', () => {
+    const actual = functions.REGEX_GROUP('^Hello, (\\w*)(!)$', 'Not present in regex', 2);
+    expect(actual).toEqual(undefined);
+  });
+});
