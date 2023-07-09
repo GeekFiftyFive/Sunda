c52475b211406ef8500e18619e1cb9db6d3482ba
GeekFiftyFive <amylouisepellatt@gmail.com>
2023-07-09 09:49:40 +0100

    Implement token pointer fundamentals

--- src/Parser/TokenPointer.test.ts
+++ src/Parser/TokenPointer.test.ts
@@ -1,10 +1,36 @@
 import { Token } from '../Tokeniser';
 import { TokenPointer } from './TokenPointer';
 
+// TODO: Find a nicer way of doing this
+const addSpanInfo = (tokens: string[]): Token[] =>
+  tokens.map((token) => ({
+    value: token,
+    pos: [0, 0],
+    line: 1,
+  }));
+
 describe('test token pointer', () => {
   test('Token pointer starts at beginning of token array', () => {
     const tokens: Token[] = [{ value: 'select', pos: [0, 6], line: 1 }];
     const tp = new TokenPointer(tokens);
     expect(tp.getCurrentToken()).toEqual(tokens[0]);
+  });
+
+  test('Token pointer can move pointer by given offset', () => {
+    const tokens: Token[] = [
+      { value: 'select', pos: [0, 6], line: 1 },
+      { value: '*', pos: [7, 8], line: 1 },
+    ];
+    const tp = new TokenPointer(tokens);
+    expect(tp.getCurrentToken()).toEqual(tokens[0]);
+    tp.move(1);
+    expect(tp.getCurrentToken()).toEqual(tokens[1]);
+  });
+
+  test('Token pointer can create segments', () => {
+    const tokens = addSpanInfo(['select', '*', 'from', 'tables']);
+    const tp = new TokenPointer(tokens);
+    const segment = tp.createSegment(1, 2);
+    expect(segment.getTokens()).toEqual([tokens[1], tokens[2]]);
   });
 });
--- src/Parser/TokenPointer.ts
+++ src/Parser/TokenPointer.ts
@@ -9,7 +9,31 @@ export class TokenPointer {
     this.tokens = tokens;
   }
 
+  private static raiseInternalError(message: string): void {
+    throw new Error(`${message}! This is an internal error, please report on GitHub :^)`);
+  }
+
+  getTokens(): Token[] {
+    return this.tokens;
+  }
+
   getCurrentToken(): Token {
     return this.tokens[this.pointer];
+  }
+
+  move(offset: number): void {
+    const newPointer = this.pointer + offset;
+    if (newPointer < 0 || newPointer >= this.tokens.length) {
+      TokenPointer.raiseInternalError('Moving pointer by this offset would exceed array bounds');
+    }
+    this.pointer = newPointer;
+  }
+
+  createSegment(startIndex: number, endIndex: number): TokenPointer {
+    if (startIndex < 0 || endIndex >= this.tokens.length) {
+      TokenPointer.raiseInternalError('Creating this segment would exceed array bounds');
+    }
+    const newTokens = this.tokens.slice(startIndex, endIndex + 1);
+    return new TokenPointer(newTokens);
   }
 }
